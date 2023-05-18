97762f2d7de2a903afbf4eeb72cd115f9dc87de4
meikee <m.kee0911@gmail.com>
2023-05-18 15:48:16 +0800

    init

--- app/src/main/java/com/example/bottomnavigation/CreatePostFragment.kt
+++ app/src/main/java/com/example/bottomnavigation/CreatePostFragment.kt
@@ -11,6 +11,7 @@ import android.os.Build
 import android.os.Bundle
 import android.provider.MediaStore
 import android.view.LayoutInflater
+import android.view.MotionEvent
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Toast
